92b57c1487f6ef6fc610e167646c4c2de204a9cf
bahaeldin <bahaa.ahmed.eng@gmail.com>
2025-10-12 17:38:07 -0700

    add skills section

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <section className="skills section" id="skills">
+        <h2 className="section__title">Skills</h2>
+        <span className="section__subtitle">My technical level</span>
+      </section>
+    </div>
+  );
+};
+
+export default Skills;
