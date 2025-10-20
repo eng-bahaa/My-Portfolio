99642df142af2e8695494dfd255cbf4cf70db203
bahaeldin <bahaa.ahmed.eng@gmail.com>
2025-10-20 07:51:11 -0700

    refine services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,6 +6,7 @@ const Services = () => {
     <section className="services section" id="services">
       <h2 className="section__title">Services</h2>
       <span className="section__subtitle">What I offer</span>
+      <div className="services__container container grid"></div>
 
       {/*  
       <div className="services__container container grid">
